5c2820a8581bc1746b3d4d655459f6de7229634a
DH2098 <kam18350@naver.com>
2026-03-18 18:47:32 +0900

    BackJoon 1000

--- BackJoon/B1000.java
+++ BackJoon/B1000.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class B1000 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();

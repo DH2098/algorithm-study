ca21c1f0f72f6fac8d18a9e2342f43a0bfc4d7af
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-19 14:44:44 +0900

    [Bronze] 10998 AxB

--- BackJoon/Bronze/B10869.java
+++ BackJoon/Bronze/B10869.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class B10869 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();

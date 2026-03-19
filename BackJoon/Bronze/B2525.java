d20a3e859fda05636ec96db56321624881380099
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-20 08:38:32 +0900

    [Bronze] 오븐 시계

--- BackJoon/Bronze/B2525.java
+++ BackJoon/Bronze/B2525.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class B2525 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();

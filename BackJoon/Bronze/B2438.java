8e9c86b90c69484c5171db6492c95858f4e6230d
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-20 13:39:37 +0900

    [Bronze] 별 찍기 - 1

---
+++ BackJoon/Bronze/B2438.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class B2438 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            sb.append("*".repeat(i)).append("\n");
+        }
+        System.out.print(sb);
+    }
+   
+}

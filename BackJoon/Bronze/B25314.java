73073e6b451a6c5062b18638c8975ec8000f6c90
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-20 10:26:07 +0900

    [Bronze] 코딩은 체육과목 입니다

---
+++ BackJoon/Bronze/B25314.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B25314 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+    
+        String result = "";
+        for(int i = 0; i < num/4; i++) {
+            result += "long ";
+        }
+        System.out.print(result + "int");    
+    }
+}

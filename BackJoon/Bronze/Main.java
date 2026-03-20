8bfc4970c6e6699cf3004cb0f4d559cd983a05b4
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-20 13:51:18 +0900

    [Bronze] 별 찍기 - 2

---
+++ BackJoon/Bronze/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n-1; i >= 0; i--) { 
+            sb.append(" ".repeat(i)).append("*".repeat(n-i)).append("\n");
+        }
+        System.out.print(sb);
+    }   
+}

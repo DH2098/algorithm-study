071c9ccdba3b0689e602e88d36b4934161f48449
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-20 13:29:55 +0900

    [Bronze] A+B - 8

---
+++ BackJoon/Bronze/B11022.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class B11022 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #")
+                .append(i + 1)
+                .append(": ")
+                .append(a)
+                .append(" + ")
+                .append(b)
+                .append(" = ")
+                .append(a+b)
+                .append("\n");
+        }
+        System.out.print(sb);
+    }
+}

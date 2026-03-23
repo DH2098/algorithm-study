7e5b2d85d4d3fc68585d940fecf2487ed4b86f09
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-23 10:00:37 +0900

    [Bronze] 아스키 코드

---
+++ BackJoon/Bronze/B11654.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class B11654 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print((int) (br.readLine().charAt(0)));
+    }
+}

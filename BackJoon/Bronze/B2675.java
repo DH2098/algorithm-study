54fc01d11dcde59ccb7c9db0a664deaa661d7c44
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-23 13:16:38 +0900

    문자열 반복

---
+++ BackJoon/Bronze/B2675.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class B2675 {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()); 
+
+            int r = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+
+            for (int j = 0; j < str.length(); j++) {
+                for (int s = 0; s < r; s++) {
+                    sb.append(str.charAt(j));
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}

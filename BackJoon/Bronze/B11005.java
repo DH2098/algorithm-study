ff21a68d308b74a0552726b2a9b2549f6bfdf627
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-26 14:05:45 +0900

    [Bronze] 진법 변환2

---
+++ BackJoon/Bronze/B11005.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class B11005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            int remainder = n % b;
+
+            if (remainder >= 10) {
+                sb.append((char) (remainder - 10 + 'A'));
+            } else {
+                sb.append(remainder);
+            }
+
+            n /= b;
+        }
+        System.out.println(sb.reverse().toString());
+    }
+}

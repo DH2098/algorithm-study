c91aebc9c7a589c4df17ec17cda8a4cbb94c4580
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-27 16:29:08 +0900

    [Bronze] 약수 구하기

---
+++ BackJoon/Bronze/B2501.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class B2501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int count = 0;  
+        int result = 0; 
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) { 
+                count++;      
+                
+                if (count == k) { 
+                    result = i;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}

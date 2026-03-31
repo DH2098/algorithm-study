af2e03ea44d1c4657c578af606c6552864c3009a
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-31 17:21:08 +0900

    [직사각형에서 탈출]

---
+++ BackJoon/Bronze/B1085.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B1085 {
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int n = ((w - x) < x)? (w - x) : x;
+        int m = ((h - y) < y)? (h - y) : y;
+
+        System.out.println((n < m)? n : m);
+    }
+}

0071e316b1eabc0b31b5256213de24ee4721e961
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-03-31 16:55:54 +0900

    [Bronze] 소인수분해

---
+++ BackJoon/Bronze/B11653.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11653 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) return;
+
+        for (int i = 2; i * i < n; i++) {
+            while (n % i == 0) {
+                System.out.println(i);
+                n /= i;
+            }
+        }
+
+        if (n > 1) System.out.println(n);
+    }
+}

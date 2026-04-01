3981d2f6d331b2f5eedbeae16428d652d735941d
DH2098 <kam18350@naver.com>
2026-04-01 10:49:20 +0900

    [Bronze] 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ BackJoon/Bronze/B25263.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class B25263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Integer.parseInt(br.readLine()));
+        System.out.println(1);
+    }    
+}

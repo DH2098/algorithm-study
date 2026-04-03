3cc5e1f984c7969190dabc88793f7d9ade3c3b4e
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-04-03 10:33:45 +0900

    [Bronze] 수 정렬하기

---
+++ BackJoon/Bronze/B2750.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class B2750 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for (int i : list) {
+            System.out.println(i);
+        }
+    }
+}

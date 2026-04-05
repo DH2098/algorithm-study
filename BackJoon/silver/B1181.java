a5a9665ef8346585ddbec19675879ebcd1ad67e0
DH2098 <kam18350@naver.com>
2026-04-05 14:29:53 +0900

    [Silver] 단어 정렬

---
+++ BackJoon/silver/B1181.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class B1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1.length() != o2.length()) {
+                return o1.length() - o2.length();
+            }
+            return o1.compareTo(o2);
+        });
+
+        System.out.println(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (!arr[i].equals(arr[i-1])) {
+                System.out.println(arr[i]);
+            }
+        }
+    }    
+}

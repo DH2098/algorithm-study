473ef0c79471dc64e9bbdb0ef5e6dc0ac6aa30ff
DH2098 <100886309+DH2098@users.noreply.github.com>
2026-04-07 16:04:53 +0900

    [Silver] 괄호

---
+++ BackJoon/silver/B9012.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class B9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            sb.append(solve(br.readLine())).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+
+    public static String solve(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+}

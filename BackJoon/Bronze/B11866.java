db5efb4d740099f0eb4b3483924ff5fd8a915d65
DH2098 <kam18350@naver.com>
2026-04-09 10:51:44 +0900

    [Silver] 요세푸스 문제 0

---
+++ BackJoon/Bronze/B11866.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class B11866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());        
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.add(i);
+        }
+
+        sb.append("<");
+
+        while (q.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                q.add(q.poll());
+            }            
+            sb.append(q.poll()).append(", ");
+        }        
+
+        sb.append(q.poll()).append(">");
+
+        System.out.println(sb);
+    }    
+}
